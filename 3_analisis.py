39682847417f2e9c1fb086ed21e083074eacca44
ARTEMISA11 <camila.mantilla@udea.edu.co>
2026-04-25 18:01:59 -0500

    Correccion recortes grafica 2 y zoom en grafica 1

--- 3_analisis.py
+++ 3_analisis.py
@@ -8,30 +8,41 @@ conexion= sqlite3.connect('arqueologia.db')
 # GRÁFICA 1: Movimiento Propio  
 df_todas = pd.read_sql_query("SELECT * FROM estrellas", conexion)
 
-plt.figure(figsize=(8, 6))
-plt.scatter(df_todas['pmRA'], df_todas['pmDE'], s=0.5, alpha=0.3, color='gray')
-plt.title('Movimiento Propio: Vía Láctea vs Omega Centauri')
-plt.xlabel('pmRA (mas/yr)')
-plt.ylabel('pmDE (mas/yr)')
-plt.xlim(-15, 10)
-plt.ylim(-15, 10)
-plt.grid(True, linestyle='--', alpha=0.6)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 7))
+
+#plt.figure(figsize=(8, 6))
+ax1.scatter(df_todas['pmRA'], df_todas['pmDE'], s=0.5, alpha=0.3, color='gray')
+ax1.set_title('Movimiento Propio: Vía Láctea vs Omega Centauri')
+ax1.set_xlabel('RA (mas/yr)')
+ax1.set_ylabel('DEC (mas/yr)')
+ax1.set_xlim(-15, 10)
+ax1.set_ylim(-15, 10)
+ax1.grid(True, linestyle='--', alpha=0.6)
+
+ax2.scatter(df_todas['pmRA'], df_todas['pmDE'], s=0.5, alpha=0.3, color='gray')
+ax2.set_title('Zoom al cumulo')
+ax2.set_xlabel('RA (mas/yr)') 
+ax2.set_ylabel('DEC (mas/yr)')
+ax2.set_xlim(-8, 2)
+ax2.set_ylim(-11, -1)
+ax2.grid(True, linestyle='--', alpha=0.6)
+
 plt.savefig('grafica1.png', dpi=300)
 print("Gráfica 1 guardada.")
 
 # GRÁFICA 2: Diagrama Color-Magnitud
-consulta= "SELECT * FROM estrellas WHERE pmRA BETWEEN -5.5 AND 0.0 AND pmDE BETWEEN -8.5 AND -4.0"
+consulta= "SELECT * FROM estrellas WHERE pmRA BETWEEN -5.0 AND -1.0 AND pmDE BETWEEN -9.0 AND -5.0"
 df_omega = pd.read_sql_query(consulta, conexion)
 
 df_omega['Color_BP_RP'] = df_omega['BPmag'] - df_omega['RPmag']
 df_omega['M_Gmag'] = df_omega['Gmag'] - 13.7 
 
 plt.figure(figsize=(6, 8))
-plt.scatter(df_omega['Color_BP_RP'], df_omega['Gmag'], s=0.8, alpha=0.5, color='darkblue')
+plt.scatter(df_omega['Color_BP_RP'], df_omega['M_Gmag'], s=0.8, alpha=0.5, color='darkblue')
 plt.gca().invert_yaxis() 
 plt.title('Diagrama HR de Omega Centauri')
-plt.xlabel(r'$BP - RP$')
-plt.ylabel(r'$G_{mag}$')
+plt.xlabel(r'$B - R$')
+plt.ylabel(r'$G_{mag}$ ABS')
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.savefig('grafica2.png', dpi=300)
 print("Gráfica 2 guardada.")
